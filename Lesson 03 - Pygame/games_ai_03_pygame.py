1cc3272126417bf7b803ed05a6b91e1ac694e09a
Milan Košťák <milankostak@seznam.cz>
2025-04-22 16:18:27 +0900

    Lesson 03 - Pygame

--- Lesson 03 - Pygame/games_ai_03_pygame.py
+++ Lesson 03 - Pygame/games_ai_03_pygame.py
@@ -54,7 +54,7 @@ def main():
         if keys[pygame.K_DOWN] and right_paddle_y < screen_height - paddle_height:
             right_paddle_y += paddle_speed
 
-        # Move ball
+        # Move the ball
         ball_x += ball_x_speed
         ball_y += ball_y_speed
 
@@ -63,7 +63,7 @@ def main():
             ball_y_speed *= -1
 
         # Ball collisions with paddles
-        left_paddle_collision = ball_x <= paddle_width and left_paddle_y < ball_y < left_paddle_y + paddle_height
+        left_paddle_collision = (ball_x <= paddle_width) and (left_paddle_y < ball_y < left_paddle_y + paddle_height)
         right_paddle_collision = (ball_x >= screen_width - paddle_width - ball_size) and \
                                  (right_paddle_y < ball_y < right_paddle_y + paddle_height)
         if left_paddle_collision or right_paddle_collision:
@@ -75,11 +75,24 @@ def main():
 
         # Drawing
         screen.fill(color=background_color)
-        pygame.draw.rect(screen, items_color, (0, left_paddle_y, paddle_width, paddle_height))
-        pygame.draw.rect(screen, items_color,
-                         (screen_width - paddle_width, right_paddle_y, paddle_width, paddle_height))
-        pygame.draw.ellipse(screen, items_color, (ball_x, ball_y, ball_size, ball_size))
-
+        # Left paddle
+        pygame.draw.rect(
+            surface=screen,
+            color=items_color,
+            rect=(0, left_paddle_y, paddle_width, paddle_height),
+        )
+        # Right paddle
+        pygame.draw.rect(
+            surface=screen,
+            color=items_color,
+            rect=(screen_width - paddle_width, right_paddle_y, paddle_width, paddle_height),
+        )
+        # Ball
+        pygame.draw.ellipse(
+            surface=screen,
+            color=items_color,
+            rect=(ball_x, ball_y, ball_size, ball_size),
+        )
         pygame.display.flip()
         pygame.time.Clock().tick(60)  # FPS
 
